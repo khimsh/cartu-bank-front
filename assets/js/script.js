99769559929a1116a727eb0fe1253001ce185ff1
khimsh <i.khimsh@gmail.com>
2022-08-04 17:14:36 +0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+})
