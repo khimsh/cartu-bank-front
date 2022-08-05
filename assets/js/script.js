e5310c5ae5aaa59b0434f9bf244a2e467c7b52b9
khimsh <i.khimsh@gmail.com>
2022-08-05 13:18:22 +0400

    index page slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.index-slider', {
     slidesPerView: 1,
+    autoplay: {
+        delay: 5000,
+    },
+    spaceBetween: 20,
+    loop: true,
+    speed: 600,
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
