b61c71ccaae269185d5976f6ad22beea903f7e5e
khimsh <i.khimsh@gmail.com>
2022-08-06 12:07:58 +0400

    started on reports

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,35 @@
-const swiper = new Swiper('.index-slider', {
-    slidesPerView: 1,
-    autoplay: {
-        delay: 5000,
-    },
-    spaceBetween: 20,
-    loop: true,
-    speed: 600,
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-})
+;(() => {
+    'use strict'
+
+    // Index page slider
+    const swiper = new Swiper('.index-slider', {
+        slidesPerView: 1,
+        autoplay: {
+            delay: 5000,
+        },
+        spaceBetween: 20,
+        loop: true,
+        speed: 600,
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    })
+
+    // Report functionality
+    function reportAccordion() {
+        if (!document.querySelector('.report')) return
+
+        const reports = document.querySelectorAll('.report')
+
+        reports.forEach((report) => {
+            const btn = report.querySelector('.report__btn')
+            btn.addEventListener('click', () => {
+                report.classList.toggle('active')
+            })
+        })
+    }
+
+    reportAccordion()
+})()
