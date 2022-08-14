a7266799d693e1fe435cd0bd25d61b62eaf0729c
khimsh <i.khimsh@gmail.com>
2022-08-14 17:14:06 +0400

    working on header dropdowns & search form

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,53 @@
             })
         })
     }
-
     reportAccordion()
+
+    // Header Nav Dropdowns
+    function headerDropdown() {
+        if (!document.querySelector('.nav')) return
+
+        const dropdowns = document.querySelectorAll('.nav__dropdown')
+
+        dropdowns.forEach((dropdown) => {
+            const btn = dropdown.querySelector('.dropdonw__trigger')
+            btn.addEventListener('click', () => {
+                dropdown.classList.toggle('active')
+            })
+        })
+    }
+    headerDropdown()
+
+    // Open Search Field
+    function headerSearchForm() {
+        if (!document.querySelector('.header__search-form')) return
+
+        const btn = document.querySelector('.header__search-btn')
+        const searchForm = document.querySelector('.header__search-form')
+
+        btn.addEventListener('click', () => {
+            searchForm.classList.toggle('active')
+        })
+    }
+    headerSearchForm()
+
+    // Remove active class from dom elements on outside click
+    document.addEventListener('click', (e) => {
+        // Remove search form
+        if (!e.target.matches('.header__search-btn, .header__search-form, .header__search-form *')) {
+            const searchForm = document.querySelector('.header__search-form')
+            if (searchForm.classList.contains('active')) {
+                searchForm.classList.remove('active')
+            }
+        }
+
+        // Remove search form
+        if (!e.target.matches('.dropdonw__trigger, .nav__content, .nav__content *')) {
+            const dropdowns = document.querySelectorAll('.nav__dropdown')
+
+            dropdowns.forEach((dropdown) => {
+                dropdown.classList.remove('active')
+            })
+        }
+    })
 })()
